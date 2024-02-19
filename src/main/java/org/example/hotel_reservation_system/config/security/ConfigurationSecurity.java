0c5abb5aeef7c247b7f4595ea8007780284d23fb
swsw <swsw>
2024-02-19 14:14:36 -0300

    feat : Spring Security configuration

---
+++ src/main/java/org/example/hotel_reservation_system/config/security/ConfigurationSecurity.java
@@ -0,0 +1,25 @@
+package org.example.hotel_reservation_system.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigurationSecurity {
+    
+    @SuppressWarnings("deprecation")
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeRequests(authorize -> authorize
+            .anyRequest().permitAll()
+        )
+        .build();
+    }
+}
