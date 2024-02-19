a7dcd51b0434e33f2767c89b7f1c15b619e36332
swsw <swsw>
2024-02-19 16:54:01 -0300

    feat : Getting BasEntity method

--- src/main/java/org/example/hotel_reservation_system/model/Clientes/ClientesEntity.java
+++ src/main/java/org/example/hotel_reservation_system/model/Clientes/ClientesEntity.java
@@ -2,24 +2,22 @@ package org.example.hotel_reservation_system.model.Clientes;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.*;
 import org.example.hotel_reservation_system.Enum.Status.StatusEnum;
+import org.example.hotel_reservation_system.model.Employees.RegisterEntity;
+import org.example.hotel_reservation_system.util.BaseEntity;
 import org.hibernate.annotations.ColumnDefault;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class ClientesEntity {
+public class ClientesEntity extends BaseEntity {
     @Id
     private String Id;
 
@@ -60,30 +58,24 @@ public class ClientesEntity {
 
     @Column(name = "data_registro")
     private LocalDateTime data_registro;
-    
 
-    /**
-     * Método chamado antes da persistência da entidade.
-     * Configura automaticamente a data de registro para o valor atual
-     * se ainda não estiver definida. Além disso, configura o ID como
-     * um UUID aleatório se ainda não estiver definido.
-     *
-     * @throws IllegalStateException Se a data de registro já estiver definida.
-     * @throws IllegalStateException Se o ID já estiver definido.
-     */
-    @PrePersist
-    public void prePersist(){
-        if (data_registro == null){
-            data_registro = LocalDateTime.now();
-        } else {
-            throw new IllegalStateException("A data de registro já está definida");
-        }
-
-        if (Id == null){
-            Id = UUID.randomUUID().toString();
-        } else {
-            throw new IllegalStateException("O Id já está definido");
-        }
+    @Override
+    public LocalDateTime getDataRegistro() {
+        return data_registro;
     }
 
+    @Override
+    public void setDataRegistro(LocalDateTime dataRegistro) {
+        this.data_registro = dataRegistro;
+    }
+
+    @Override
+    public String getId() {
+        return Id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.Id = id;
+    }
 }
