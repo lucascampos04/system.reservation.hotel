544c42708a3045ef91b3d5b18b87d8d939adfff2
swsw <swsw>
2024-02-24 13:40:17 -0300

    feat : Enum of plans created

---
+++ src/main/java/org/example/hotel_reservation_system/Enum/Planos/TipoPlanoEnum.java
@@ -0,0 +1,24 @@
+package org.example.hotel_reservation_system.Enum.Planos;
+
+import lombok.Getter;
+
+@Getter
+public enum TipoPlanoEnum {
+    BASICO("Plano Básico"),
+    PADRAO("Plano Padrão"),
+    EXECUTIVO("Plano Executivo"),
+    NEGOCIOS("Plano de Negócios"),
+    LUXO("Plano de Luxo"),
+    ROMANCE("Pacote de Romance"),
+    FAMILIA("Pacote Família"),
+    LONGA_ESTADIA("Plano de Longa Estadia"),
+    VIP("Pacote VIP"),
+    COBERTURA("Plano de Cobertura");
+
+    private final String descricao;
+
+    TipoPlanoEnum(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
