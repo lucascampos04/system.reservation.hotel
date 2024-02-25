57aeb1e95264b307fa37b389a950dcf7b24e874c
swsw <swsw>
2024-02-24 22:42:25 -0300

    refactor : new roles

--- src/main/java/org/example/hotel_reservation_system/Enum/roles/RolesEnum.java
+++ src/main/java/org/example/hotel_reservation_system/Enum/roles/RolesEnum.java
@@ -1,7 +1,11 @@
 package org.example.hotel_reservation_system.Enum.roles;
 
 public enum RolesEnum {
-    ROLE_CLIENTE,
-    ROLE_FUNCIONARIO,
-    ROLE_ADMIN
+    SEM_PLANO,
+    ROLE_CLIENTE_BASICO,
+    ROLE_CLIENTE_EXECUTIVO,
+    ROLE_CLIENTE_LUXO,
+    ROLE_FUNCIONARIO_GERENCIA,
+    ROLE_FUNCIONARIO_LIMPEZA,
+    ROLE_ADMINISTRADOR_HOTEL
 }
