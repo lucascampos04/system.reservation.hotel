749d77fd58e3f4fa7ea1e7c11c4bf2edb94b183d
lucascampos04 <camposdlucasoli@gmail.com>
2024-03-16 16:31:29 -0300

    feat : logic to log changes before and after client upgrade.

--- src/main/java/org/example/hotel_reservation_system/services/Cliente/Patch/PatchServices.java
+++ src/main/java/org/example/hotel_reservation_system/services/Cliente/Patch/PatchServices.java
@@ -34,44 +34,72 @@ public class PatchServices {
     }
 
     private void applyPartialUpdate(ClientesEntity cliente, ClientesDto updates) {
+        StringBuilder change = new StringBuilder("Alterações realizadas: \n");
+
+        String oldNome = cliente.getNome();
+        String oldEmail = cliente.getEmail();
+        String oldCpf = cliente.getCpf();
+        String oldRg = cliente.getRg();
+        String oldEndereco = cliente.getEndereco();
+        String oldCep = cliente.getCep();
+        String oldNumero = cliente.getNumero();
+        String oldEstado = cliente.getEstado();
+        String oldPais = cliente.getPais();
+        String oldData_nascimento = cliente.getData_nascimento();
+        String oldStatus = String.valueOf(cliente.getStatus());
+        String oldRole = String.valueOf(cliente.getRole());
+
         if (updates.getNome() != null) {
+            change.append("Nome: ").append(oldNome).append(" -> ").append(updates.getNome()).append("\n");
             cliente.setNome(updates.getNome());
         }
         if (updates.getEmail() != null) {
+            change.append("Email: ").append(oldEmail).append(" -> ").append(updates.getEmail()).append("\n");
             cliente.setEmail(updates.getEmail());
         }
         if (updates.getCpf() != null) {
+            change.append("CPF: ").append(oldCpf).append(" -> ").append(updates.getCpf()).append("\n");
             cliente.setCpf(updates.getCpf());
         }
         if (updates.getRg() != null) {
+            change.append("RG: ").append(oldRg).append(" -> ").append(updates.getRg()).append("\n");
             cliente.setRg(updates.getRg());
         }
         if (updates.getEndereco() != null) {
+            change.append("Endereço: ").append(oldEndereco).append(" -> ").append(updates.getEndereco()).append("\n");
             cliente.setEndereco(updates.getEndereco());
         }
         if (updates.getCep() != null) {
+            change.append("CEP: ").append(oldCep).append(" -> ").append(updates.getCep()).append("\n");
             cliente.setCep(updates.getCep());
         }
         if (updates.getNumero() != null) {
+            change.append("Número: ").append(oldNumero).append(" -> ").append(updates.getNumero()).append("\n");
             cliente.setNumero(updates.getNumero());
         }
         if (updates.getEstado() != null) {
+            change.append("Estado: ").append(oldEstado).append(" -> ").append(updates.getEstado()).append("\n");
             cliente.setEstado(updates.getEstado());
         }
         if (updates.getPais() != null) {
+            change.append("País: ").append(oldPais).append(" -> ").append(updates.getPais()).append("\n");
             cliente.setPais(updates.getPais());
         }
         if (updates.getData_nascimento() != null) {
+            change.append("Data de nascimento: ").append(oldData_nascimento).append(" -> ").append(updates.getData_nascimento()).append("\n");
             cliente.setData_nascimento(updates.getData_nascimento());
         }
         if (updates.getStatus() != null) {
+            change.append("Status: ").append(oldStatus).append(" -> ").append(updates.getStatus()).append("\n");
             cliente.setStatus(updates.getStatus());
         }
-        if (updates.getData_registro() != null) {
-            cliente.setData_registro(updates.getData_registro());
-        }
         if (updates.getRole() != null) {
+            change.append("Role: ").append(oldRole).append(" -> ").append(updates.getRole()).append("\n");
             cliente.setRole(updates.getRole());
+        }
+
+        if (!change.toString().equals("Alterações realizadas: \n")){
+            System.out.println(change.toString());
         }
     }
 
