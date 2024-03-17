e9e4992bbf46580388bcd8d3061989eaeff26a26
lucascampos04 <camposdlucasoli@gmail.com>
2024-03-17 13:29:26 -0300

    feat: starting table reservas

---
+++ src/main/java/org/example/hotel_reservation_system/Enum/Status/StatusPagamentoEnum.java
@@ -0,0 +1,9 @@
+package org.example.hotel_reservation_system.Enum.Status;
+
+public enum StatusPagamentoEnum {
+    PENDENTE,
+    PROCESSANDO,
+    CONCLUIDO,
+    CANCELADO,
+    FALHA
+}
---
+++ src/main/java/org/example/hotel_reservation_system/model/Reservas/ReservasEntity.java
@@ -0,0 +1,35 @@
+package org.example.hotel_reservation_system.model.Reservas;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.hotel_reservation_system.Enum.Status.StatusPagamentoEnum;
+import org.example.hotel_reservation_system.model.Clientes.ClientesEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reservas_entity")
+public class ReservasEntity {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "forma_pagamento")
+    private String formaPagamento;
+
+    @Column(name = "valor")
+    private Double valor;
+
+    @Column(name = "data_pagamento")
+    private LocalDateTime dataPagamento;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private StatusPagamentoEnum statusPagamento;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private ClientesEntity clientes;
+}
