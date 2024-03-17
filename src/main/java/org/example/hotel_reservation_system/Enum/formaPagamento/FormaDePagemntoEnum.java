ab091b3fe93dc4debd984139e874316d816ead61
lucascampos04 <camposdlucasoli@gmail.com>
2024-03-17 13:52:28 -0300

    feat: enum forma de pagamento

---
+++ src/main/java/org/example/hotel_reservation_system/Enum/formaPagamento/FormaDePagemntoEnum.java
@@ -0,0 +1,11 @@
+package org.example.hotel_reservation_system.Enum.formaPagamento;
+
+public enum FormaDePagemntoEnum {
+    DINHEIRO,
+    CARTAO_CREDITO,
+    CARTAO_DEBITO,
+    TRANSFERENCIA_BANCARIA,
+    BOLETO_BANCARIO,
+    CHEQUE,
+    PIX,
+}
