db1b56186afb18841d3d687165be1edfaa6abbd2
lucascampos04 <camposdlucasoli@gmail.com>
2024-03-23 19:56:54 -0300

    feat : new queries

--- sql/Main.sql
+++ sql/Main.sql
@@ -1,29 +1,15 @@
-\connect ManagementHoteis;
-select * from clientes;
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'employees';
 
-delete from clientes
-where id IN (
-	select id
-	from clientes
-	where plano_id is null
-);
-
-select email, plano_id from clientes;
+select column_name, data_type
+from information_schema.columns
+where table_name = 'reservas_entity';
 
 select * from clientes;
-select email from clientes;
 select * from plano_entity;
 
-delete from plano_entity where pacote is null;
-
-delete from clientes 
-where plano_id in (
-	select c.plano_id 
-	from clientes c
-	left join plano_entity p on c.plano_id = p.id
-	where p.pacote is null
-);
-
-select id, status from clientes;
-
-select * from reservas_entity;
+SELECT c.nome, c.email, c.cpf,
+       p.plano, p.valor
+FROM clientes c
+JOIN plano_entity p ON c.plano_id = p.id;
