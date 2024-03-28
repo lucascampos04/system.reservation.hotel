07e5dfd6a039ad261e30671aeab703cbe067a8d6
lucascampos04 <camposdlucasoli@gmail.com>
2024-03-28 17:03:42 -0300

    Code update, removing reservations

--- src/main/java/org/example/hotel_reservation_system/repository/Reservas/ReservasRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.hotel_reservation_system.repository.Reservas;
-
-import org.example.hotel_reservation_system.model.Reservas.ReservasEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservasRepository extends JpaRepository<ReservasEntity, Long> {
-}
