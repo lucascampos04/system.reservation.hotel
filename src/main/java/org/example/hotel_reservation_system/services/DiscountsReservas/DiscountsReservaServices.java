33043e2d33678a426d28fd1147c12d42e5485e9e
lucascampos04 <camposdlucasoli@gmail.com>
2024-04-06 20:02:48 -0300

    feat: applying design pattern “strategy”

--- src/main/java/org/example/hotel_reservation_system/services/DiscountsReservas/DiscountsReservaServices.java
+++ src/main/java/org/example/hotel_reservation_system/services/DiscountsReservas/DiscountsReservaServices.java
@@ -2,19 +2,37 @@ package org.example.hotel_reservation_system.services.DiscountsReservas;
 
 import org.example.hotel_reservation_system.Enum.Planos.TipoPlanoEnum;
 import org.springframework.stereotype.Service;
+
 @Service
 public class DiscountsReservaServices {
+    private final DiscountStrategy basicDiscountStrategy;
+    private final DiscountStrategy familyDiscountStrategy;
+    private final DiscountStrategy luxuryDiscountStrategy;
 
-    public static Double applyDiscount(Double valor, TipoPlanoEnum plano) {
-        if (plano.equals(TipoPlanoEnum.BASICO) || plano.equals(TipoPlanoEnum.PADRAO)){
-            return valor - (valor * 0.05);
-        } else if (plano.equals(TipoPlanoEnum.FAMILIA) || plano.equals(TipoPlanoEnum.LONGA_ESTADIA)){
-            return valor - (valor * 0.1);
-        } else if (plano.equals(TipoPlanoEnum.LUXO) || plano.equals(TipoPlanoEnum.EXECUTIVO) || plano.equals(TipoPlanoEnum.VIP)){
-            return valor - (valor * 0.15);
-        } else {
-            return valor;
+    public DiscountsReservaServices(
+             DiscountStrategy basicDiscountStrategy,
+             DiscountStrategy familyDiscountStrategy,
+             DiscountStrategy luxuryDiscountStrategy) {
+        this.basicDiscountStrategy = basicDiscountStrategy;
+        this.familyDiscountStrategy = familyDiscountStrategy;
+        this.luxuryDiscountStrategy = luxuryDiscountStrategy;
+    }
+
+    public Double applyDiscount(Double valor, TipoPlanoEnum plano) {
+        switch (plano) {
+            case BASICO:
+            case PADRAO:
+                return basicDiscountStrategy.aplyDiscount(valor);
+            case FAMILIA:
+            case LONGA_ESTADIA:
+                return familyDiscountStrategy.aplyDiscount(valor);
+            case LUXO:
+            case EXECUTIVO:
+            case VIP:
+                return luxuryDiscountStrategy.aplyDiscount(valor);
+            default:
+                return valor;
         }
+
     }
 }
-
