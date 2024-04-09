d9f948176ad1e09592194785d293e6740c64ab6a
lucascampos04 <camposdlucasoli@gmail.com>
2024-04-09 19:07:11 -0300

    fix: employees are being displayed successfully

--- src/main/java/org/example/hotel_reservation_system/services/Employees/post/add/AddEmployeesServices.java
+++ src/main/java/org/example/hotel_reservation_system/services/Employees/post/add/AddEmployeesServices.java
@@ -6,6 +6,7 @@ import org.example.hotel_reservation_system.Enum.Status.StatusEmployees;
 import org.example.hotel_reservation_system.dto.Employees.EmployeesDto;
 import org.example.hotel_reservation_system.model.Employees.EmployeesEntity;
 import org.example.hotel_reservation_system.repository.Employees.EmployeesRepository;
+import org.example.hotel_reservation_system.services.EmailServices.Employees.EmployeeSuccessfullyHired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,13 +16,23 @@ import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AddEmployeesServices {
-    @Autowired
-    private EmployeesRepository employeesRepository;
+
+    private final EmployeesRepository employeesRepository;
+    private final EmployeeSuccessfullyHired employeeSuccessfullyHired;
+    public AddEmployeesServices(EmployeesRepository employeesRepository, EmployeeSuccessfullyHired employeeSuccessfullyHired) {
+        this.employeesRepository = employeesRepository;
+        this.employeeSuccessfullyHired = employeeSuccessfullyHired;
+    }
 
     public ResponseEntity<String> addEmployees(EmployeesDto employeesDto) {
         try{
             EmployeesEntity employeesEntity = getDads(employeesDto);
             employeesRepository.save(employeesEntity);
+
+            if (employeesEntity != null) {
+                employeeSuccessfullyHired.SendEmailEmployeeHired(employeesEntity.getEmail(), employeesEntity.getNome(), employeesEntity.getCargo());
+            }
+
             return ResponseEntity.ok("Funcionaário adicionado com sucesso");
         } catch (Exception e){
             e.printStackTrace();
@@ -44,10 +55,10 @@ public class AddEmployeesServices {
         employeesEntity.setPais(employeesDto.getPais());
         employeesEntity.setDataNascimeo(employeesDto.getDataNascimeo());
         employeesEntity.setStatus(StatusEmployees.ATIVO);
-        employeesEntity.setCargo(CargoEmployees.GERENTE);
+        employeesEntity.setCargo(employeesDto.getCargo());
         employeesEntity.setData_registro(LocalDateTime.now());
-        employeesEntity.setSalario(1500.0);
-        employeesEntity.setContratos(ContratosEmployees.UM_ANO);
+        employeesEntity.setSalario(employeesDto.getSalario());
+        employeesEntity.setContratos(employeesDto.getContratos());
         return employeesEntity;
     }
 
